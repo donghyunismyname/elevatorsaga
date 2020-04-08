adde1cc177b9d66f22f137a15f18f5bf5cbe3d43
Donghyun <klimdhn@kaist.ac.kr>
2020-04-09 01:10:18 +0900

    asfaafdasd

--- elevatorsaga.js
+++ elevatorsaga.js
@@ -1,57 +1,85 @@
 {
     init: function(elevators, floors) {
-        function getFloorsOn(floors) {
-            ret = []
-            for (f in floors) ret[f] = floors[f].up_button || floors[f].down_button;
+        function pickRandomInArray(arr) {
+            return arr[Math.floor(Math.random() * arr.length)];
+        }
+        function upElevator() {
+            ret = pickRandomInArray(elevators.filter(e => e.goingUpIndicator()));
+            return ret==undefined ? pickRandomInArray(elevators) : ret;
+        }
+        function downElevator() {
+            ret = pickRandomInArray(elevators.filter(e => e.goingDownIndicator()));
+            return ret==undefined ? pickRandomInArray(elevators) : ret;
+        }
+        function minLoadFactorElevator() {
+            ret = elevators[0]
+            for (elevator of elevators) {
+                ret = ret.loadFactor() < elevator.loadFactor() ? ret : elevator;
+            }
+            return ret;
+        }
+
+        upWaitingFloors = []
+        downWaitingFloors = []
+        function arrange(elevator) {
+            console.assert(elevator.destinationDirection() == "stopped");
+            ct = new Date().getTime();
+
+
+            U = elevator.getPressedFloors().filter(f => f > elevator.currentFloor());
+            D = elevator.getPressedFloors().filter(f => f < elevator.currentFloor());
+            if (elevator.loadFactor() < 0.6) {
+                U = U.concat(upWaitingFloors.filter(f => f > elevator.currentFloor()));
+                D = D.concat(downWaitingFloors.filter(f => f < elevator.currentFloor()));
+            }
+            u = Math.min(...U);
+            d = Math.min(...D);
+
+            goUp = elevator.currentFloor() == floor.length-1 ? false :
+                   elevator.currentFloor() == 0              ? true :
+                   U.length==0 ? false :
+                   D.length==0 ? true :
+                   Math.abs(elevator.currentFloor() - u) < Math.abs(elevator.currentFloor() - d);
+            elevator.goingUpIndicator(goUp);
+            elevator.goingDownIndicator(!goUp);
+
+            if (U.length==0 && D.length==0) return;
+
+            dest = goUp ? u : d;
+            elevator.goToFloor(dest);
+            console.log(dest);
+
+            if (elevator.loadFactor() < 0.6) {
+                if (goUp)
+                    upWaitingFloors = upWaitingFloors.filter(f => f != dest);
+                else
+                    downWaitingFloors = downWaitingFloors.filter(f => f != dest);
+            }
         }
 
         for (elevator of elevators) {
-            elevator.on("idle", function() {
-                floorsOn = getFloorsOn();
- or (e in elevators) {
-            var elevator = elevators[e];
-               
-            });
-
-            //elevator.on("passing_floor", foo);
-
-            elevator.on("stopped_at_floor", function(for (f in floorNums) {
-                console.log("stopped_at_floor", = floorNum);
-                if (this.dir == 1)
-                    floors[floorNum].up_button = false;
-                else (this.dir == -1)
-                    floors[floorNum].down_button = false;s[f]
-                floor.on("up_button_pressed", function() {
+            elevator.goingDownIndicator(false);
+            elevator.on("stopped_at_floor", function(floorNum) {
+                arrange(this);
             });
         }
 
         for (floor of floors) {
-            floor.up_button = false;
-            floor.down_button = false;
-elevator.goToFloor(this.level)
-                });
-                floor.on("updown_button_pressed", function() {
-                console.log("up_button_pressed", this.floorNum());
-                this.up_button = true;    elevator.goToFloor(this.level)
-                });
+            floor.on("up_button_pressed", function() {
+                upWaitingFloors.push([this.floorNum(), new Date().getTime()]);
+                for (elevator of elevators)
+                    if (elevator.destinationDirection() == "stopped")
+                        arrange(elevator);
             });
-
-
-            floelevator.on("downfloor_button_pressed", function(floorNum) { 
-                console.log("down_button_pressed", this.floorNum()elevator.goToFloor(floorNum)
+            floor.on("down_button_pressed", function() {
+                downWaitingFloors.push([this.floorNum(), new Date().getTime()]);
+                for (elevator of elevators)
+                    if (elevator.destinationDirection() == "stopped")
+                        arrange(elevator);
             });
-        }
-        this.down_button = true;
-        
-        
-    });,
         }
     },
-    
-    update: function(dt, elevators, floors) {},
-        // We normally don't need to do anything here
-    }
+
+
+    update: function(dt, elevators, floors) {}
 }
-<!--stackedit_data:
-eyJoaXN0b3J5IjpbLTE2MTcwMzg1ODhdfQ==
--->
